95cb4d58f014130f3dbadef34b4c502f39a7e27a
Sid Mann <sjmann2@outlook.com>
2022-12-02 15:49:08 -0600

    test feat spend points class method

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,28 @@
 class Transaction < ApplicationRecord
   validates_presence_of :payer, :points
   validates_numericality_of :points
+
+  def self.spend_points(total_points)
+    response = Hash.new(0)
+    Transaction.order(:timestamp).map do |transaction|
+      if transaction.points > total_points
+        response[transaction.payer]  -= total_points
+        Transaction.create!(payer: transaction.payer, points: -total_points, timestamp: DateTime.now)
+
+        total_points = 0
+      else
+        Transaction.create!(payer: transaction.payer, points: -transaction.points, timestamp: DateTime.now)
+        response[transaction.payer] -= transaction.points
+        total_points -= transaction.points
+      end
+
+      if total_points.zero?
+        break
+      end
+    end
+    response.map do |payer, points|
+      {:payer => payer, :points => points}
+    end
+  end
 end
+#possibly add column to transactions that is "points spent"
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -6,4 +6,22 @@ RSpec.describe Transaction, type: :model do
     it { should validate_presence_of :points }
     it { should validate_numericality_of :points }
   end
+
+  describe 'class methods' do
+    describe 'spend_points' do
+      let!(:transaction_1) { Transaction.create!(payer: "DANNON", points: 300, timestamp: DateTime.new(2022,10,31,10)) }
+      let!(:transaction_2) { Transaction.create!(payer: "UNILEVER", points: 200, timestamp: DateTime.new(2022,10,31,11)) }
+      let!(:transaction_3) { Transaction.create!(payer: "DANNON", points: -200, timestamp: DateTime.new(2022,10,31,15)) }
+      let!(:transaction_4) { Transaction.create!(payer: "MILLER COORS", points: 10000, timestamp: DateTime.new(2022,11,01,14)) }
+      let!(:transaction_5) { Transaction.create!(payer: "DANNON", points: 1000, timestamp: DateTime.new(2022,11,02,14)) }
+  
+      it 'updates transactions starting with oldest timestamp to remove points until the points balance is zero' do
+        expect(Transaction.spend_points(5000)).to eq([
+          { "payer": "DANNON", "points": -100 },
+          { "payer": "UNILEVER", "points": -200 },
+          { "payer": "MILLER COORS", "points": -4700 }
+          ])
+        end
+    end
+  end
 end
