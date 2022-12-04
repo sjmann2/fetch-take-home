3aa4ea06b7f290cd854fb618ef464edb84f0986e
Sid Mann <sjmann2@outlook.com>
2022-12-04 17:22:55 -0600

    test edge cases, update spend points method with comments

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,37 +1,43 @@
 class Transaction < ApplicationRecord
-  validates_presence_of :payer, :points
-  validates_numericality_of :points
+  validates_presence_of :payer, :points_initial, :points_available
+  validates_numericality_of :points_initial, :points_available
 
-  def self.spend_points(total_points)
+  def self.spend_points(points_to_redeem)
     response = Hash.new(0)
-    Transaction.order(:timestamp).map do |transaction|
-      if transaction.points > total_points
-        response[transaction.payer]  -= total_points
-        Transaction.create!(payer: transaction.payer, points: -total_points, timestamp: DateTime.now)
-
-        total_points = 0
+    # grabbing transactions that have available points and ordering by oldest first
+    Transaction.where("points_available > 0").order(:timestamp).map do |transaction|
+      # if the transaction has more points available than requested, points redeemed is equal to the points requested
+      if transaction.points_available > points_to_redeem
+        points_redeemed = points_to_redeem
       else
-        Transaction.create!(payer: transaction.payer, points: -transaction.points, timestamp: DateTime.now)
-        response[transaction.payer] -= transaction.points
-        total_points -= transaction.points
+      # otherwise, points redeemed is equal to the amount of points available in that transaction
+        points_redeemed = transaction.points_available
       end
-
-      if total_points.zero?
+      # updating the transaction to account for the points spent
+      transaction.update!(points_available: transaction.points_available - points_redeemed)
+      # associating the payer with the amount of points spent
+      response[transaction.payer] -= points_redeemed
+      # subtract points redeemed from the total amount requested
+      points_to_redeem -= points_redeemed
+      #once points requested is at zero stop the cycle
+      if points_to_redeem.zero?
         break
       end
     end
+    # create a new transaction with the total amount of points subtracted from the payer, return payer, points
     response.map do |payer, points|
+      Transaction.create!(payer: payer, points_initial: -points, points_available: 0, timestamp: DateTime.now)
       {:payer => payer, :points => points}
     end
-    #possibly add column to transactions that is "points spent"--prevent re spending points
   end
 
-  def self.sufficient_points?(points_redeemed)
-    points_redeemed < Transaction.sum(:points)
+  def self.sufficient_points?(points_to_redeem)
+    # check if there are enough points to redeem the amount of points requested
+    points_to_redeem < Transaction.where("points_available > 0").sum(:points_available)
   end
 
 
   def self.get_points
-    Transaction.group(:payer).sum(:points)
+    Transaction.group(:payer).sum(:points_available)
   end
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -3,51 +3,64 @@ require 'rails_helper'
 RSpec.describe Transaction, type: :model do
   describe 'validations' do
     it { should validate_presence_of :payer }
-    it { should validate_presence_of :points }
-    it { should validate_numericality_of :points }
+    it { should validate_presence_of :points_initial }
+    it { should validate_presence_of :points_available }
+    it { should validate_numericality_of :points_initial }
+    it { should validate_numericality_of :points_available }
   end
 
   describe 'class methods' do
     describe 'spend_points' do
-      let!(:transaction_1) { Transaction.create!(payer: "DANNON", points: 300, timestamp: DateTime.new(2022,10,31,10)) }
-      let!(:transaction_2) { Transaction.create!(payer: "UNILEVER", points: 200, timestamp: DateTime.new(2022,10,31,11)) }
-      let!(:transaction_3) { Transaction.create!(payer: "DANNON", points: -200, timestamp: DateTime.new(2022,10,31,15)) }
-      let!(:transaction_4) { Transaction.create!(payer: "MILLER COORS", points: 10000, timestamp: DateTime.new(2022,11,01,14)) }
-      let!(:transaction_5) { Transaction.create!(payer: "DANNON", points: 1000, timestamp: DateTime.new(2022,11,02,14)) }
+      let!(:transaction_1) { Transaction.create!(payer: "DANNON", points_initial: 300, points_available: 100, timestamp: DateTime.new(2022,10,31,10)) }
+      let!(:transaction_2) { Transaction.create!(payer: "UNILEVER", points_initial: 200, points_available: 200, timestamp: DateTime.new(2022,10,31,11)) }
+      let!(:transaction_3) { Transaction.create!(payer: "DANNON", points_initial: -200, points_available: 0, timestamp: DateTime.new(2022,10,31,15)) }
+      let!(:transaction_4) { Transaction.create!(payer: "MILLER COORS", points_initial: 10000, points_available: 10000, timestamp: DateTime.new(2022,11,01,14)) }
+      let!(:transaction_5) { Transaction.create!(payer: "DANNON", points_initial: 1000, points_available: 1000, timestamp: DateTime.new(2022,11,02,14)) }
   
-      it 'updates transactions starting with oldest timestamp to remove points until the points balance is zero' do
+      it 'updates transactions starting with oldest timestamp to redeem requested points, update available points, and return points spent' do
         expect(Transaction.spend_points(5000)).to eq([
           { "payer": "DANNON", "points": -100 },
           { "payer": "UNILEVER", "points": -200 },
           { "payer": "MILLER COORS", "points": -4700 }
-          ])
+          ]) 
+
+        expect(Transaction.order(:timestamp).first.points_available).to eq(0)
+      end
+
+      it 'does not spend the same points twice' do
+        expect(Transaction.spend_points(100)).to eq([
+          { "payer": "DANNON", "points": -100 }
+          ]) 
+        expect(Transaction.spend_points(100)).to eq([
+          { "payer": "UNILEVER", "points": -100 }
+          ]) 
       end
     end
 
     describe 'insufficient_points?' do
-      let!(:transaction_1) { Transaction.create!(payer: "DANNON", points: 300, timestamp: DateTime.new(2022,10,31,10)) }
-      let!(:transaction_2) { Transaction.create!(payer: "UNILEVER", points: 200, timestamp: DateTime.new(2022,10,31,11)) }
-      let!(:transaction_3) { Transaction.create!(payer: "DANNON", points: -200, timestamp: DateTime.new(2022,10,31,15)) }
-      let!(:transaction_4) { Transaction.create!(payer: "MILLER COORS", points: 10000, timestamp: DateTime.new(2022,11,01,14)) }
-      let!(:transaction_5) { Transaction.create!(payer: "DANNON", points: 1000, timestamp: DateTime.new(2022,12,01,14)) }
-      it 'returns false if there are not enough points to fulfill the request' do
+      let!(:transaction_1) { Transaction.create!(payer: "DANNON", points_initial: 300, points_available: 300, timestamp: DateTime.new(2022,10,31,10)) }
+      let!(:transaction_2) { Transaction.create!(payer: "UNILEVER", points_initial: 200, points_available: 200, timestamp: DateTime.new(2022,10,31,11)) }
+      let!(:transaction_3) { Transaction.create!(payer: "DANNON", points_initial: -200, points_available: 0, timestamp: DateTime.new(2022,10,31,15)) }
+      let!(:transaction_4) { Transaction.create!(payer: "MILLER COORS", points_initial: 10000, points_available: 10000, timestamp: DateTime.new(2022,11,01,14)) }
+      let!(:transaction_5) { Transaction.create!(payer: "DANNON", points_initial: 1000, points_available: 1000, timestamp: DateTime.new(2022,12,01,14)) }
+      it 'returns false if there are not enough available points to fulfill the request' do
         expect(Transaction.sufficient_points?(50000)).to eq(false)
       end
 
-      it 'returns true if there are enough points to fulfill the request' do
+      it 'returns true if there are enough available points to fulfill the request' do
         expect(Transaction.sufficient_points?(5000)).to eq(true)
       end
     end
 
     describe 'get_points' do
-      let!(:transaction_1) { Transaction.create!(payer: "DANNON", points: 300, timestamp: DateTime.new(2022,10,31,10)) }
-      let!(:transaction_2) { Transaction.create!(payer: "UNILEVER", points: 200, timestamp: DateTime.new(2022,10,31,11)) }
-      let!(:transaction_3) { Transaction.create!(payer: "DANNON", points: -200, timestamp: DateTime.new(2022,10,31,15)) }
-      let!(:transaction_4) { Transaction.create!(payer: "MILLER COORS", points: 10000, timestamp: DateTime.new(2022,11,01,14)) }
-      let!(:transaction_5) { Transaction.create!(payer: "DANNON", points: 1000, timestamp: DateTime.new(2022,12,01,14)) }
-      let!(:transaction_6) { Transaction.create!(payer: "DANNON", points: -100, timestamp: DateTime.new(2022,12,01,14)) }
-      let!(:transaction_7) { Transaction.create!(payer: "UNILEVER", points: -200, timestamp: DateTime.new(2022,12,01,14)) }
-      let!(:transaction_8) { Transaction.create!(payer: "MILLER COORS", points: -4700, timestamp: DateTime.new(2022,12,01,14)) }
+      let!(:transaction_1) { Transaction.create!(payer: "DANNON", points_initial: 300, points_available: 0, timestamp: DateTime.new(2022,10,31,10)) }
+      let!(:transaction_2) { Transaction.create!(payer: "UNILEVER", points_initial: 200, points_available: 0, timestamp: DateTime.new(2022,10,31,11)) }
+      let!(:transaction_3) { Transaction.create!(payer: "DANNON", points_initial: -200, points_available: 0, timestamp: DateTime.new(2022,10,31,15)) }
+      let!(:transaction_4) { Transaction.create!(payer: "MILLER COORS", points_initial: 10000, points_available: 5300, timestamp: DateTime.new(2022,11,01,14)) }
+      let!(:transaction_5) { Transaction.create!(payer: "DANNON", points_initial: 1000, points_available: 1000, timestamp: DateTime.new(2022,12,01,14)) }
+      let!(:transaction_6) { Transaction.create!(payer: "DANNON", points_initial: -100, points_available: 0, timestamp: DateTime.new(2022,12,01,14)) }
+      let!(:transaction_7) { Transaction.create!(payer: "UNILEVER", points_initial: -200, points_available: 0, timestamp: DateTime.new(2022,12,01,14)) }
+      let!(:transaction_8) { Transaction.create!(payer: "MILLER COORS", points_initial: -4700, points_available: 0, timestamp: DateTime.new(2022,12,01,14)) }
       it 'returns all payers and their current total number of points' do
         expect(Transaction.get_points).to eq(
           {
